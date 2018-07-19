99c66a2a67bc50c1ee02c00491a16f6b9b55a7a1
Rajbir Singh <rajbir.singh@opin.ca>
2018-07-19 12:55:43 -0400

    removing System.out.print statements from code

--- src/act_news/News.java
+++ src/act_news/News.java
@@ -17,41 +17,40 @@ import act_users.ReadCSV;
 
 public class News {
 
-	 public static void top3news(WebDriver driver,Logger logger, String top3news) {
-			// check for top 3 news on the Dashboard
-			String top_news = "";
-			try {
-				top_news = top_news + driver
-						.findElement(By.cssSelector(
-								".view--dashboard-latest-3.view--display-block_1 .view__row  article  h3"))
-						.getText().toString();
-			} catch (Exception e) {
+	public static void top3news(WebDriver driver, Logger logger, String top3news) {
+		// check for top 3 news on the Dashboard
+		String top_news = "";
+		try {
+			top_news = top_news + driver
+					.findElement(
+							By.cssSelector(".view--dashboard-latest-3.view--display-block_1 .view__row  article  h3"))
+					.getText().toString();
+		} catch (Exception e) {
 
-			}
-			try {
-				top_news = top_news + "/";
-				top_news = top_news + driver.findElement(By.cssSelector(
-						".view--dashboard-latest-3.view--display-block_2 .view__row:nth-child(1)  article  h3"))
-						.getText().toString();
-			} catch (Exception e) {
+		}
+		try {
+			top_news = top_news + "/";
+			top_news = top_news + driver
+					.findElement(By.cssSelector(
+							".view--dashboard-latest-3.view--display-block_2 .view__row:nth-child(1)  article  h3"))
+					.getText().toString();
+		} catch (Exception e) {
 
-			}
-			try {
-				top_news = top_news + "/";
-				top_news = top_news + driver.findElement(By.cssSelector(
-						".view--dashboard-latest-3.view--display-block_2 .view__row:nth-child(2)  article  h3"))
-						.getText().toString();
-			} catch (Exception e) {
+		}
+		try {
+			top_news = top_news + "/";
+			top_news = top_news + driver
+					.findElement(By.cssSelector(
+							".view--dashboard-latest-3.view--display-block_2 .view__row:nth-child(2)  article  h3"))
+					.getText().toString();
+		} catch (Exception e) {
 
-			}
-   System.out.println(top_news);
-   System.out.println(top3news);
-			if (top_news.equals(top3news)) {
-				logger.info("Top 3 dashboard news test: PASSED " + "\n");
-			} else {
-				logger.info("Top 3 dashboard news test: FAILED " + "\n");
-			}
-	   }
-	   
+		}
+		if (top_news.equals(top3news)) {
+			logger.info("Top 3 dashboard news test: PASSED " + "\n");
+		} else {
+			logger.info("Top 3 dashboard news test: FAILED " + "\n");
+		}
+	}
 
 }
